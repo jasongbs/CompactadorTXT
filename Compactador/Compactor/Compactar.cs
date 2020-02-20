eb14e229cfcb2955e47d04a0b1ec2f2d20911919
Jason Gabriel <jasongbds@gmail.com>
2020-02-20 02:28:18 -0300

    criado a ordenacao no arquivo dicionario

--- Compactador/Compactor/Compactar.cs
+++ Compactador/Compactor/Compactar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Compactador.Services;
 
@@ -10,17 +11,17 @@ namespace Compactador.Compactor
     {
         public void Compacta(string text, string compactado)
         {
-            string[] descompactado = File.ReadAllLines(text,Encoding.UTF7);
-         
+            string[] descompactado = File.ReadAllLines(text, Encoding.UTF7);
+
             // string dirDicionario = File.ReadAllText(new DirDicionario().PathDirData(), Encoding.UTF8);
-            string textonovo="",textNovo = "", dicionario = "";
-          
-            var existe = new List<string> { };
+            string textonovo = "", textNovo = "", dicionario = "";
+
+            var existe = new List<string> { "`"};
             int posicao;
 
             foreach (string line in descompactado)
             {
-                textonovo += line+" ´ ";
+                textonovo += line + " ´ ";
             }
             string[] palavras = textonovo.Split(" ");
             /*Verifica se a palavra existe no dicionario, se não existir ele insere a mesma na area de dicionario
@@ -30,22 +31,69 @@ namespace Compactador.Compactor
             {
                 if (existe.IndexOf(line) == -1)//verifica se a palavra não existe no dicionario
                 {
-                    existe.Add(line);//adiciona a palavra no dicionário
-                    dicionario = string.Concat(dicionario + line + " ");//atualiza o dicionário
+                    if (line.Contains(','))
+                    {
+                        string[] separa = line.Split(',');
+                        if (existe.IndexOf(",") >= 0)
+                        {
+                            existe.Add(separa[0]);
+                        }
+                        else if (existe.IndexOf(",") == -1)
+                        {
+                            existe.Add(separa[0]);
+                            existe.Add(",");
+                        }
+                    }
+                    else if (line.Contains('.'))
+                    {
+                        string[] separa = line.Split('.');
+                        if (existe.IndexOf(".") >= 0)
+                        {
+                            existe.Add(separa[0]);
+                        }
+                        else if (existe.IndexOf(".") == -1)
+                        {
+                            existe.Add(separa[0]);
+                            existe.Add(".");
+                        }
+                    }
+                    else
+                        existe.Add(line);//adiciona a palavra no dicionário
+                    //dicionario = dicionario.OrderBy().ToList();
+                    existe = existe.OrderBy(dicionario => dicionario).ToList();
+
+
                 }
+            }
+
+            foreach (string line in existe)
+                dicionario = string.Concat(dicionario + line + " ");//atualiza o dicionário,
+
+            foreach (string line in palavras)
+            {
                 if (existe.Equals("´"))
                 {
-                    textNovo = string.Concat(textNovo+ "\n ");
+                    textNovo = string.Concat(textNovo + "\n ");
                 }
-                posicao = existe.IndexOf(line);//verifica a posição da palavra
-                textNovo = string.Concat(textNovo + posicao + " ");//E insere a posição na area de texto compactado
+                if (line.IndexOf(",") >= 0 || line.IndexOf(".") >= 0)
+                    textNovo = Verifica(line, existe, textNovo);
+                else
+                {
+                    posicao = existe.IndexOf(line);//verifica a posição da palavra
+                    textNovo = string.Concat(textNovo + posicao + " ");//E insere a posição na area de texto compactado
+                }
             }
             //Cria arquivo txt com o texto compactado
-            File.WriteAllText(compactado,textNovo);
+            File.WriteAllText(compactado, textNovo);
 
             //biblioteca
             string caminho = new DirDicionario().PathDirData();
+//modo 1
             File.WriteAllText(caminho, dicionario);
+//modo 2
+           /* StreamWriter s = File.AppendText(caminho);
+            s.WriteLine(dicionario);
+            s.Close();*/
 
             FileStream original = new FileStream(text, FileMode.Open);
             FileStream compacto = new FileStream(compactado, FileMode.Open);
@@ -64,6 +112,48 @@ namespace Compactador.Compactor
                 original.Close();
                 compacto.Close();
             }
+        }
+
+        private string Verifica(string line, List<string> existe, string textNovo)
+        {
+            int posicao;
+            if (line.Contains(','))
+            {
+                string[] separa = line.Split(',');
+                if (existe.IndexOf(",") >= 1)
+                {
+                    posicao = existe.IndexOf(separa[0]);//verifica a posição da palavra
+                    textNovo = string.Concat(textNovo + posicao + " ");//E insere a posição na area de texto compactado
+                    return textNovo;
+                }
+                else if (existe.IndexOf(",") == -1)
+                {
+                    posicao = existe.IndexOf(separa[0]);//verifica a posição da palavra
+                    textNovo = string.Concat(textNovo + posicao + " ");//E insere a posição na area de texto compactado
+                    posicao = existe.IndexOf(",");//verifica a posição da palavra
+                    textNovo = string.Concat(textNovo + posicao + " ");//E insere a posição na area de texto compactado
+                    return textNovo;
+                }
+            }
+            else if (line.Contains('.'))
+            {
+                string[] separa = line.Split('.');
+                if (existe.IndexOf(".") >= 0)
+                {
+                     posicao = existe.IndexOf(separa[0]);//verifica a posição da palavra
+                    textNovo = string.Concat(textNovo + posicao + " ");//E insere a posição na area de texto compactado
+                    return (textNovo);
+                }
+                else if (existe.IndexOf(".") == -1)
+                {
+                     posicao = existe.IndexOf(separa[0]);//verifica a posição da palavra
+                    textNovo = string.Concat(textNovo + posicao + " ");//E insere a posição na area de texto compactado
+                    posicao = existe.IndexOf(".");//verifica a posição da palavra
+                    textNovo = string.Concat(textNovo + posicao + " ");//E insere a posição na area de texto compactado
+                    return (textNovo);
+                }
+            }
+            return textNovo;
         }
     }
 }
---
+++ Compactador/Services/OrdenaDicionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compactador.Services
+{
+    class OrdenaDicionario
+    {
+
+        /*--- Método de Ordenação do vetor de string------------------------------------------------------*/
+
+        static void Selecao(string[] palavra)
+        {
+            int i, j, min;
+            string varAux;
+            for (i = 0; i < palavra.Length - 1; i++)
+            {
+                min = i;
+                for (j = i + 1; j < palavra.Length; j++)
+                {
+                    // Utilizando o CompareTo para comparar as string do vetor
+                    // resultado -1 significa que cidades[j] < cidades[min]
+                    if (palavra[j].CompareTo(palavra[min]) == -1)
+                    {
+                        min = j;
+                    }
+                }
+                varAux = palavra[min];
+                palavra[min] = palavra[i];
+                palavra[i] = varAux;
+            }
+
+
+        }
+    }
+}
